3061241da687ff9979917bcf003409b04e726643
Angie Camelo <acamelo@consensussa.co>
2023-11-19 15:17:59 -0500

    Adición funcionalidad para mostrar información.

--- car.js
+++ car.js
@@ -1,5 +1,12 @@
 var myCar = {
     "marca": "Chevrolet",
     "modelo" : "2018",
-    "color": "gris"
+    "color": "gris",
+    "mostrarInformacion": function() {
+        console.log("Información del automóvil:");
+        console.log("Marca: " + this.marca);
+        console.log("Modelo: " + this.modelo);
+        console.log("Color: " + this.color);
+    }
 }
+myCar.mostrarInformacion();
